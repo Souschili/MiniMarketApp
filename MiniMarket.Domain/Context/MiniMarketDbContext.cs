bd28bd10d808ba9a27bda3cac8005a1e225d4eb0
Orkhan <aliyevorhan82@gmail.com>
2024-11-07 00:15:06 +0400

    добавил контекст

---
+++ MiniMarket.Domain/Context/MiniMarketDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MiniMarket.Domain.Entity;
+using System.Data;
+
+namespace MiniMarket.Domain.Context
+{
+    public class MiniMarketDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+
+        public MiniMarketDbContext(DbContextOptions<MiniMarketDbContext> options) : base(options)
+        {
+            // без миграций
+            Database.EnsureCreated();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var tracker = base.ChangeTracker.Entries<BaseEntity>();
+            foreach (var entity in tracker)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    entity.Entity.CreatedAt = DateTime.UtcNow;
+                    entity.Entity.UpdatedAt = null;
+                }
+                else if (entity.State == EntityState.Modified)
+                {
+                    entity.Entity.UpdatedAt = DateTime.UtcNow;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
