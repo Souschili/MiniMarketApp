0013639f4a2ccfee2b7c799c3dfdf2391f6085cc
Orkhan <aliyevorhan82@gmail.com>
2024-11-06 23:59:31 +0400

    add product model

---
+++ MiniMarket.Domain/Entity/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniMarket.Domain.Entity
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
