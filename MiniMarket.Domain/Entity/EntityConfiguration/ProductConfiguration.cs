6031a632f3012e363913e520d508d91a2ced1983
Orkhan <aliyevorhan82@gmail.com>
2024-11-07 01:43:01 +0400

    закончил конфигурацию модели

---
+++ MiniMarket.Domain/Entity/EntityConfiguration/ProductConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace MiniMarket.Domain.Entity.EntityConfiguration
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(nameof(Product));
+
+            builder.Property<int>("Id")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Price)
+            .HasColumnType("decimal(18,2)")  // Устанавливаем точность для decimal
+            .IsRequired();  // Указываем, что поле обязательно
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Category)
+                .HasMaxLength(50)
+                .HasDefaultValue("None");
+
+        }
+    }
+}
