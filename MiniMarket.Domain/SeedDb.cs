12c77d80c0a1a1de7290046baf2e5e16db2e5a40
Orkhan <aliyevorhan82@gmail.com>
2024-11-10 23:12:56 +0400

    add fake product generator

---
+++ MiniMarket.Domain/SeedDb.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using MiniMarket.Domain.Context;
+using MiniMarket.Domain.Entity;
+
+namespace MiniMarket.Domain
+{
+    public class SeedDb
+    {
+        private readonly MiniMarketDbContext _context;
+        public SeedDb(MiniMarketDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void StartSeed()
+        {
+            // если есть продуеты то ничего не делаем
+            if (!_context.Products.Any())
+            {
+                return;
+            }
+
+            // тут генерация продуктов и добавочных таблиц
+            var product = GenerateProduct();
+        }
+
+        private IEnumerable<Product> GenerateProduct()
+        {
+            decimal minPrice = 0.1m;
+            decimal maxPrice = 10000m;
+            var fakeProduct = new Faker<Product>()
+                .RuleFor(p => p.Name, f => f.Commerce.Product())
+                .RuleFor(p => p.Price, f => Decimal.Parse(f.Commerce.Price(minPrice, maxPrice)))
+                .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0]);
+
+            var products= fakeProduct.Generate(100);
+            return products;
+        }
+    }
+}
