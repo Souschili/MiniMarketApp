0adee7d7370f256c0997116854b9d8e63211a334
Orkhan <aliyevorhan82@gmail.com>
2024-11-11 19:39:26 +0400

    добавил сущность юзер

---
+++ MiniMarket.Domain/Entity/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MiniMarket.Domain.Entity.EntityConfiguration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(x=> x.Login)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
---
+++ MiniMarket.Domain/Entity/User.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniMarket.Domain.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+    }
+}
