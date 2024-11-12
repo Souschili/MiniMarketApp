d506a52b0604bc77cd91be81e2e50e77c33860ce
Orkhan <aliyevorhan82@gmail.com>
2024-11-13 03:18:32 +0400

    add standart repo

---
+++ MiniMarket.Domain/Repositories/CartItemRepo.cs
@@ -0,0 +1,12 @@
+﻿using MiniMarket.Domain.Context;
+using MiniMarket.Domain.Entity;
+using MiniMarket.Domain.Repositories.Contract;
+
+namespace MiniMarket.Domain.Repositories
+{
+    internal class CartItemRepo : GenericRepo<CartItem>, ICartItem
+    {
+        public CartItemRepo(MiniMarketDbContext context) : base(context) { }
+      
+    }
+}
---
+++ MiniMarket.Domain/Repositories/CartRepo.cs
@@ -0,0 +1,12 @@
+﻿using MiniMarket.Domain.Context;
+using MiniMarket.Domain.Entity;
+using MiniMarket.Domain.Repositories.Contract;
+
+namespace MiniMarket.Domain.Repositories
+{
+    internal class CartRepo : GenericRepo<Cart>, ICartRepo
+    {
+        public CartRepo(MiniMarketDbContext context) : base(context) { }
+        
+    }
+}
