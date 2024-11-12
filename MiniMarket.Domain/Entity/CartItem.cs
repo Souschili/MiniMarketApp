2b044d8b12e4ce3c97d8e2d035aea1253fdc7d8d
Orkhan <aliyevorhan82@gmail.com>
2024-11-13 00:18:01 +0400

    добавил карту и айтемы для карты

---
+++ MiniMarket.Domain/Entity/CartItem.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniMarket.Domain.Entity
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }  // ID корзины, к которой привязан товар
+        public Cart Cart { get; set; }   // Навигационное свойство для связи с Cart
+
+        public int ProductId { get; set; }  // ID товара
+        public Product Product { get; set; } // Навигационное свойство для связи с Product
+
+        public int Quantity { get; set; }  // Количество товара
+    }
+
+}
